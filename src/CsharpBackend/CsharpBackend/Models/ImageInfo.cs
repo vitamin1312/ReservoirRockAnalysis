4ea72b2db381f935e9af1f3d495667d900f9c545
vitamin1312 <vitamin20021312@gmail.com>
2024-10-23 18:22:18 +0300

    Added ImageInfo class

---
+++ src/CsharpBackend/CsharpBackend/Models/ImageInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpBackend.Models
+{
+    public class ImageInfo
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? UploadDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateOnly? CreationDate { get; set; }
+
+        public int? FieldId { get; set; }
+        virtual public Field? Field { get; set; }
+
+        public int CoreSampleImageId { get; set; }
+
+        public CoreSampleImage image { get; set; }
+
+
+    }
+}
