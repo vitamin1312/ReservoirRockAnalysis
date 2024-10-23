d46e1ba56c8359e5828b79468e51bc629d9f85df
vitamin1312 <vitamin20021312@gmail.com>
2024-10-23 16:24:02 +0300

    Added CSharpBackend

---
+++ src/CsharpBackend/CsharpBackend/Models/CoreSampleImage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CsharpBackend.Models
+{
+    public class CoreSampleImage
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime UploadDate { get; set; }
+
+        [DataType(DataType.Date)] 
+        public DateOnly? CreationDate { get; set; }
+
+        public string PathToImage { get; set; }
+
+        public string? PathToMask { get; set; }
+
+        public int? FieldId { get; set; }
+        virtual public Field? Field { get; set; }
+
+    }
+}
---
+++ src/CsharpBackend/CsharpBackend/Models/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace CsharpBackend.Models
+{
+    public class Field
+    {
+        public int Id { get; set; }
+
+        public List<CoreSampleImage> FieldImages { get; set; }
+    }
+}
