8d12693abb5be7aab2fddd6d6d8b951be2f3f829
vitamin1312 <vitamin20021312@gmail.com>
2024-12-11 01:10:08 +0300

    Added header and footer

---
+++ src/AngularFrontend/angular-frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {}
