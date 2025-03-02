Fixed criteria computation. Added system callbacks

src/NeuralNetwork/BlissLearn/BlissLearner.py
@@ -10,11 +10,17 @@ else:
     from tqdm import tqdm
 
 from CallbackState import CallbackState
+from SystemBlissCallbacks import LossCallback
 from BlissTypes import (criterion,
                         nullable_scheduler,
                         optimizer_type,
                         step_function,
-                        nullable_callbacks_list)
+                        nullable_callbacks_list,
+                        nullable_system_callbacks_list,
+                        nullable_system_callbacks_getter)
+
+def get_system_callbacks() -> nullable_system_callbacks_list:
+    return [LossCallback()]
 
 
 class BlissLearner:
@@ -26,6 +32,7 @@ class BlissLearner:
                  train_dataloader: DataLoader,
                  test_dataloader: DataLoader,
                  callbacks: nullable_callbacks_list = None,
+                 system_callbacks_getter: nullable_system_callbacks_getter = get_system_callbacks,
 
                  # todo: add next fields using
                  scheduler: nullable_scheduler = None,
@@ -36,10 +43,16 @@ class BlissLearner:
         
         self.model = model
         self.loss_function = loss_function
+
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
+
         self.optimizer = optimizer_class(model.parameters(), **optimizer_kwargs)
+
         self.callbacks = callbacks if callbacks else []
+        system_callbacks = system_callbacks_getter()
+        self.system_callbacks = system_callbacks if system_callbacks else []
+
         self.callback_state = CallbackState()
 
     ################### Loss calculation ###################
@@ -62,20 +75,17 @@ class BlissLearner:
         loss, outputs = self.calc_loss_with_grad(xb, yb)
         self.optimizer.step()
         with torch.no_grad():
-            for callback in self.callbacks:
-                callback.on_train_batch_end(yb, outputs, self.callback_state)
+            for callback in self.system_callbacks: callback.on_train_batch_end(loss.item(), self.callback_state)
+            for callback in self.callbacks: callback.on_train_batch_end(yb, outputs, self.callback_state)
 
-        self.callback_state.train_loss_function_values.append(loss.item())
         return loss.item()
 
     def validate_step(self, xb: torch.Tensor, yb: torch.Tensor) -> float:
         with torch.no_grad():
             loss, outputs = self.calc_loss(xb, yb)
+            for callback in self.system_callbacks: callback.on_eval_batch_end(loss.item(), self.callback_state)
+            for callback in self.callbacks: callback.on_eval_batch_end(yb, outputs, self.callback_state)
 
-            for callback in self.callbacks:
-                callback.on_eval_batch_end(yb, outputs, self.callback_state)
-
-        self.callback_state.eval_loss_function_values.append(loss.item())
         return loss.item()
 
     ################### Train loop ###################
@@ -86,16 +96,19 @@ class BlissLearner:
             sys.stdout.flush()
 
             self.do_epoch(self.train_step, self.train_dataloader)
+            for callback in self.system_callbacks: callback.on_train_epoch_end(self.callback_state)
             for callback in self.callbacks: callback.on_train_epoch_end(self.callback_state)
 
             print(f'validating', flush=True)
             sys.stdout.flush()
 
             self.do_epoch(self.validate_step, self.test_dataloader)
+            for callback in self.system_callbacks: callback.on_eval_epoch_end(self.callback_state)
             for callback in self.callbacks: callback.on_eval_epoch_end(self.callback_state)
 
     # https://stackoverflow.com/questions/64727187/tqdm-multiple-progress-bars-with-nested-for-loops-in-pycharm
-    def do_epoch(self, batch_processing: step_function, dataloader: DataLoader) -> None:
+    @staticmethod
+    def do_epoch(batch_processing: step_function, dataloader: DataLoader) -> None:
         for batch_number, (xb, yb) in tqdm(
                 enumerate(dataloader),
                 position=1,

src/NeuralNetwork/BlissLearn/BlissTypes.py
@@ -4,7 +4,7 @@ from typing import Callable, Union, Type
 from torch.optim.lr_scheduler import _LRScheduler
 from torch import optim
 
-from BlissCallback import BlissCallback
+from BlissCallback import BlissCallback, BlissSystemCallback
 
 # Python
 nullable_int = Union[None, int]
@@ -17,8 +17,10 @@ nullable_scheduler = Union[None, _LRScheduler]
 # Functions
 step_function = Callable[[torch.Tensor, torch.Tensor], float]
 criterion = Callable[[tensor_or_scaler, tensor_or_scaler], tensor_or_scaler]
+nullable_system_callbacks_getter = Callable[[], Union[None, list[BlissSystemCallback]]]
 
 # Collections
 nullable_list = Union[None, list]
 nullable_criterion_dict = Union[None, dict[str, criterion]]
 nullable_callbacks_list = Union[None, list[BlissCallback]]
+nullable_system_callbacks_list = Union[None, list[BlissSystemCallback]]

src/NeuralNetwork/BlissLearn/CustomBlissCallbacks.py
@@ -1,11 +1,11 @@
-import sys
-from collections import defaultdict
 import torch
 
 from BlissCallback import BlissCallback
 from BlissTypes import nullable_criterion_dict
 from CallbackState import CallbackState
 
+
+################### Classification Callback ###################
 class SegmentationMetricsCallback(BlissCallback):
     def __init__(
             self,
@@ -28,7 +28,7 @@ class SegmentationMetricsCallback(BlissCallback):
         class_metrics_values = dict()
         for name, metric in self.class_metrics.items():
             for class_num in range(self.num_classes):
-                class_outputs = torch.where(outputs == class_num, 1, 0).argmax(axis=1)
+                class_outputs = torch.where(outputs == class_num, 1, 0).argmax(dim=1)
                 class_targets = torch.where(yb == class_num, 1, 0)
                 class_metrics_values[name + f'class{class_num}'] = metric(class_outputs, class_targets)
         return class_metrics_values
@@ -51,7 +51,7 @@ class SegmentationMetricsCallback(BlissCallback):
         metrics = self.on_batch_end(yb, outputs)
 
         for name, value in metrics.items():
-            callback_state.batch_train_criteria[name].append(value)
+            callback_state.update_criteria(name, value, train=True)
 
     def on_eval_batch_end(self,
                            yb: torch.Tensor,
@@ -61,7 +61,7 @@ class SegmentationMetricsCallback(BlissCallback):
         metrics = self.on_batch_end(yb, outputs)
 
         for name, value in metrics.items():
-            callback_state.batch_eval_criteria[name].append(value)
+            callback_state.update_criteria(name, value, train=False)
 
 
     def on_train_epoch_end(self,
@@ -73,10 +73,11 @@ class SegmentationMetricsCallback(BlissCallback):
     def on_eval_epoch_end(self,
                      callback_state,
                      *args, **kwargs) -> None:
-        callback_state.accumulate_train_batch_to_epoch()
-        callback_state.clear_train_batch_criteria()
+        callback_state.accumulate_eval_batch_to_epoch()
+        callback_state.clear_eval_batch_criteria()
 
 
+################### Print Callback ###################
 class PrintCriteriaCallback(BlissCallback):
     def on_train_batch_end(self,
                      yb: torch.Tensor,
@@ -95,7 +96,7 @@ class PrintCriteriaCallback(BlissCallback):
     def on_train_epoch_end(self,
                      callback_state,
                      *args, **kwargs) -> None:
-        print(f'loss_function: {callback_state.train_loss_function_values[-1]:.3f}')
+        print(f'loss_function: {callback_state.epoch_train_loss[-1]:.3f}')
         for criteria_name, value in callback_state.get_last_train_criteria_values().items():
             print(f'{criteria_name}: {value:.3f}')
         print()
@@ -103,7 +104,7 @@ class PrintCriteriaCallback(BlissCallback):
     def on_eval_epoch_end(self,
                      callback_state,
                      *args, **kwargs) -> None:
-        print(f'loss_function: {callback_state.eval_loss_function_values[-1]:.3f}')
+        print(f'loss_function: {callback_state.epoch_eval_loss[-1]:.3f}')
         for criteria_name, value in callback_state.get_last_eval_criteria_values().items():
             print(f'{criteria_name}: {value:.3f}')
         print()