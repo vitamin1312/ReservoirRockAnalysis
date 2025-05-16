f963ec9704501631f04dc8a6cace1604806afb1c
vitamin1312 <vitamin20021312@gmail.com>
2025-05-16 03:23:49 +0300

    Test for new functions

--- src/CsharpBackend/TestBackend/UnitTest1.cs
+++ src/CsharpBackend/TestBackend/UnitTest1.cs
@@ -3,6 +3,9 @@ using Emgu.CV;
 using CsharpBackend.NeuralNetwork;
 using Emgu.CV.CvEnum;
 using CsharpBackend.Utils;
+using CsharpBackend.Config;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace TestBackend
 {
@@ -11,17 +14,38 @@ namespace TestBackend
         [Fact]
         public async void Test1()
         {
-            int size = (int)(1024 * 3.5);
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // достаём конфиг
+            var appConfig = config.GetSection("AppConfig").Get<AppConfig>();
+            if (appConfig == null)
+                throw new InvalidOperationException("There are no app config");
+
+            // читаем метаданные
+            var poreClassesJson = File.ReadAllText(appConfig.PathToPoreClasses);
+            var poreClasses = JsonConvert.DeserializeObject<List<PoreClasses>>(poreClassesJson)?.FirstOrDefault();
+
+            var poreColorsJson = File.ReadAllText(appConfig.PathToPoreColors);
+            var poreColors = JsonConvert.DeserializeObject<PoreColors>(poreColorsJson);
+
+            if (poreClasses == null || poreColors == null)
+                throw new InvalidOperationException("Error while loading metadata (pore colors and pore classes)");
+
+            // инициализируем
+            DataConverter.Init(poreClasses, poreColors);
+            int size = (int)(1024);
             var neuralNetwork = new NeuralNetwork(
                 @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\resources\unetppforcsharp.onnx",
                 size
             );
             var coreSampleImage = new CoreSampleImage()
             {
-                PathToImage = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\data\images\Kondur3_122842_1.jpg"
+                PathToImage = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\data\train-test\segmentation-train\image0318261_4.png"
             };
 
-            var image = coreSampleImage.GetImageMat();
+            var image = DataConverter.GetImageMat(coreSampleImage.PathToImage);
 
             //image = NeuralNetwork.ResizeImage(image, 400, 200, 3);
 
@@ -29,8 +53,8 @@ namespace TestBackend
             var mask = await NeuralNetwork.ProcessImageWithNN(inputImage);
             var maskResized = DataConverter.ResizeImage(mask, image.Cols, image.Rows, 3);
 
-            var pathToImage = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\data\test\image.jpg";
-            var pathToMask = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\data\test\mask.png";
+            var pathToImage = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\wwwroot\ImageFiles\image.png";
+            var pathToMask = @"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\wwwroot\ImageFiles\mask.png";
 
             image.Save(pathToImage);
             maskResized.Save(pathToMask);
@@ -38,14 +62,20 @@ namespace TestBackend
             coreSampleImage.PathToImage = pathToImage;
             coreSampleImage.PathToMask = pathToMask;
 
-            var Image = coreSampleImage.GetImageMat();
-            var Mask = coreSampleImage.GetMaskMat();
-            var MaskImage = coreSampleImage.GetMaskImageMat();
-            var ImageWithMask = coreSampleImage.GetImageWithMaskMat();
-            CvInvoke.Imshow("image", Image);
-            CvInvoke.Imshow("mask", Mask * 255 / 5);
-            CvInvoke.Imshow("maskImage", MaskImage);
-            CvInvoke.Imshow("ImageWithMask", ImageWithMask);
+            var Image = DataConverter.GetImageMat(coreSampleImage.PathToImage);
+            var Mask = DataConverter.GetMaskMat(coreSampleImage.PathToImage);
+            var MaskImage = DataConverter.GetMaskImageMat(coreSampleImage.PathToMask);
+            var ImageWithMask = DataConverter.GetImageWithMaskMat(coreSampleImage.PathToImage, coreSampleImage.PathToMask);
+            //CvInvoke.Imshow("image", Image);
+            //CvInvoke.Imshow("mask", Mask * 255 / 5);
+            //CvInvoke.Imshow("maskImage", MaskImage);
+            //CvInvoke.Imshow("ImageWithMask", ImageWithMask);
+            //CvInvoke.WaitKey(0);
+
+            var RealMaskImage = new Mat(@"C:\Users\Viktor\Documents\IT\ReservoirRockAnalysis\data\Images\Sihor\image_masks\318261_2.png");
+            RealMaskImage = DataConverter.BgraBinarization(RealMaskImage);
+            RealMaskImage = DataConverter.ConvertBgra2Bgr(RealMaskImage);
+            CvInvoke.Imshow("RealMaskImage", RealMaskImage);
             CvInvoke.WaitKey(0);
 
         }
