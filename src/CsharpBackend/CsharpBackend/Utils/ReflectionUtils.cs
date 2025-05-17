346a76a927149591e9f561f58ab9bd967d2bc91f
vitamin1312 <vitamin20021312@gmail.com>
2025-05-17 04:26:45 +0300

    belated commit)

---
+++ src/CsharpBackend/CsharpBackend/Utils/ReflectionUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace CsharpBackend.Utils
+{
+    public class ReflectionUtils
+    {
+        public static IEnumerable<string> GetPublicProperties<T>()
+        {
+            return typeof(T)
+                .GetProperties()
+                .Where(x => x.GetMethod?.IsPublic == true)
+                .Select(x => x.Name);
+        }
+
+        public static object? GetPropertyValue(object obj, string propertyName)
+        {
+            if (obj == null || string.IsNullOrWhiteSpace(propertyName))
+                return null;
+
+            var propInfo = obj.GetType().GetProperty(propertyName);
+
+            return propInfo?.GetValue(obj);
+        }
+    }
+}
